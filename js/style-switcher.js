7984950d31045de60834e186bef019d51ac4dfd7
JosueAparicio <josue9aparicio@gmail.com>
2021-03-04 20:55:35 -0600

    js

---
+++ js/style-switcher.js
@@ -0,0 +1,52 @@
+/* ------------------toggle  style switcher -------------------*/
+const styleSwitcherToggler = document.querySelector('.style-switcher-toggler');
+styleSwitcherToggler.addEventListener('click', ()=> {
+    document.querySelector('.style-switcher').classList.toggle('open');
+})
+
+//hide style switcher on scroll
+window.addEventListener('scroll', ()=>{
+    if(document.querySelector('.style-switcher').classList.contains('open')){
+        document.querySelector('.style-switcher').classList.remove('open')
+    }
+})
+
+/**---------------theme colors ------------------------- */
+const alternateStyle = document.querySelectorAll('.alternate-style');
+function setActiveStyle(color){
+    alternateStyle.forEach((theme) =>{
+        if(color == theme.getAttribute('title')){
+            theme.removeAttribute('disabled');
+        }else{
+            theme.setAttribute('disabled', 'true');
+
+        }
+    })
+}
+
+/** -------- theme light and dark mode ----------------*/
+const dayNight = document.querySelector('.day-night');
+dayNight.addEventListener('click', ()=>{
+    dayNight.querySelector('i').classList.toggle('fa-sun');
+    dayNight.querySelector('i').classList.toggle('fa-moon');
+    document.body.classList.toggle('dark');
+})
+window.addEventListener('load', ()=>{
+    if(document.body.classList.contains('dark')){
+        dayNight.querySelector('i').classList.add('fa-sun');
+    }else{
+        dayNight.querySelector('i').classList.add('fa-moon');
+    }
+})
+
+/**----------------change lang-------------------------- */
+const changeLang = document.querySelector('.change-lang');
+changeLang.addEventListener('click', ()=>{
+
+    if(window.location.pathname == '/en/index.html'){
+        window.location.pathname = '/index.html';
+    }else{
+        window.location.pathname = '/en/index.html';
+    }
+
+})
